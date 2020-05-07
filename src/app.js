f77c487d1fa515a46156c939064f90d7af6b8d1e
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-07 17:20:25 +0200

    fetch real aggregated data

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 import './app.scss'
-import fetchData from './plugins/ods-context.js'
-import generateCards from './components/kpi-card.js'
+import loadDataNetworkFirst from './plugins/ods-context.js'
+// import generateCards from './components/kpi-card.js'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     const registration = await navigator.serviceWorker.register('/sw.js')
     try {
       console.log(`Service Worker registered! Scope: ${registration.scope}`)
-    } catch(err) {
+    } catch (err) {
       console.log(`Service Worker registration failed: ${err}`)
     }
   })
 }
 
+const generateCards = (data) => console.log(data)
 
-fetchData()
-generateCards()
+const getDataAndRenderUi = async () => {
+  const data = await loadDataNetworkFirst()
+  generateCards(data)
+}
+
+getDataAndRenderUi()
--- src/plugins/ods-context.js
+++ src/plugins/ods-context.js
@@ -1,10 +1,11 @@
 /* global fetch */
 import { openDB } from 'idb'
 
-const baseUrl = 'https://public.opendatasoft.com/api/v2/catalog/datasets/donnees-hospitalieres-covid-19-dep-france/records'
+const baseUrl = 'https://public.opendatasoft.com/api/v2/catalog/datasets/donnees-hospitalieres-covid-19-dep-france/'
+const query = "aggregates?select=sum(day_hosp) as sum_hos, sum(day_intcare) as sum_intcare, sum(tot_death) as sum_death, sum(tot_out) as sum_out&where=sex like 'Tous'&group_by=date&order_by=-date&limit=1"
 
 const getServerData = async () => {
-  const networkRes = await fetch(baseUrl)
+  const networkRes = await fetch(baseUrl + query)
   const networkData = networkRes.json()
   return networkData
 }
@@ -12,10 +13,6 @@ const getServerData = async () => {
 const getLocalData = async (db) => {
   if (!('indexedDB' in window)) { return null }
   return db.getAll('hosp')
-}
-
-const upDateUi = (msg, data) => {
-  console.log(msg, data)
 }
 
 const noDataMsg = async () => {
@@ -35,19 +32,20 @@ const createIndexedDB = async () => {
   return db
 }
 
-export default async () => {
+export default async (updateUiFn) => {
   if (!('indexedDB' in window)) { return null }
   const db = await createIndexedDB()
   try {
     const networkData = await getServerData()
     await db.clear('hosp')
     const tx = db.transaction('hosp', 'readwrite')
-    networkData.records.forEach(record => tx.store.add(record))
+    networkData.aggregations.forEach(record => tx.store.add(record))
     await tx.done
-    upDateUi('From Network :', networkData)
-  } catch {
+    return networkData
+  } catch (err) {
     console.log('Network request failed. Offline mode !')
+    console.error(err)
     const offlineData = await getLocalData(db)
-    offlineData ? upDateUi('From cache', offlineData) : noDataMsg()
+    offlineData || noDataMsg()
   }
 }
